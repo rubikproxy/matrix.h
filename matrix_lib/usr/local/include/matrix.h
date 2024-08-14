c65656da99182dd8452a5b2ad1468d7f22004ebb
SANJAY KUMAR <84948167+rubikproxy@users.noreply.github.com>
2024-08-14 20:10:28 +0530

    Update matrix.h

--- matrix_lib/usr/local/include/matrix.h
+++ matrix_lib/usr/local/include/matrix.h
@@ -9,7 +9,6 @@ typedef struct {
     double *data;
 } Matrix;
 
-// Basic operations
 Matrix *create_matrix(size_t rows, size_t cols);
 void free_matrix(Matrix *matrix);
 void print_matrix(const Matrix *matrix);
@@ -19,12 +18,11 @@ Matrix *matrix_mul(const Matrix *a, const Matrix *b);
 Matrix *matrix_transpose(const Matrix *matrix);
 double matrix_determinant(const Matrix *matrix);
 
-// Advanced operations
+
 Matrix *matrix_identity(size_t size);
 double matrix_norm(const Matrix *matrix);
 void matrix_eigenvalues(const Matrix *matrix, double *real, double *imag);
 
-// Error handling
 const char *matrix_error_message(void);
 
-#endif // MATRIX_H
+#endif 
